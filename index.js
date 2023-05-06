991f43bb3c0040698005770d314564f98e2f42e0
StoyanBanov <sbanov05@gmail.com>
2023-05-06 22:34:44 +0300

    add socket.io config

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const databaseConfig = require('./config/database')
 const expressConfig = require('./config/express')
 const routes = require('./config/routes')
+const socketIoConfig = require('./config/socketIo')
 const { port } = require('./variables')
 
 const app = require('express')()
+const server = require('http').createServer(app)
 
 start()
 
 async function start() {
     await databaseConfig(app)
+    socketIoConfig(server)
     expressConfig(app)
     routes(app)
 
-    app.listen(process.env.PORT || port, () => console.log(`Listening at ${port}`))
+    server.listen(process.env.PORT || port, () => console.log(`Listening at ${port}`))
 }
