f50f6aaae1d7cbd3dd4533fc781b440bbdbe7b29
StoyanBanov <sbanov05@gmail.com>
2023-06-18 15:18:55 +0300

    add profile picture preview

--- static/scripts/profile.js
+++ static/scripts/profile.js
@@ -5,6 +5,23 @@ import { handlePostAction } from "/static/scripts/postActionsUtil.js"
 const list = document.getElementById('profileList')
 const listOptions = document.getElementById('profileListOptions')
 
+const profilePicFromInput = document.getElementById('profilePicFromInput')
+
+document.getElementById('profilePicInput').addEventListener('change', e => {
+    const file = e.target.files[0];
+    if (file) {
+        let fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+            document.getElementById('profilePicFromInput').src = fileReader.result
+        }
+    }
+})
+
+profilePicFromInput.addEventListener('click', e => {
+    //TODO
+})
+
 scrollWindow(list, 'showPosts', 1)
 
 listOptions.addEventListener('click', async e => {
